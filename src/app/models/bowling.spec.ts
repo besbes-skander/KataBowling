5ec3aa91690b4f2c701719432a97664a99e9df65
Skander <besbes.skander@gmail.com>
2021-06-07 10:50:54 +0200

    add rolls

--- src/app/models/bowling.spec.ts
+++ src/app/models/bowling.spec.ts
@@ -1,4 +1,4 @@
-import { Bowling } from './bowling';
+import {Bowling} from './bowling';
 
 describe('Bowling', () => {
   it('should create an instance', () => {
@@ -8,5 +8,15 @@ describe('Bowling', () => {
   it('should return 0 if never scored', () => {
     const bowlingGame = new Bowling();
     expect(bowlingGame.getScore()).toEqual(0);
+  });
+
+  it('should return 20 if scored 1 pts in each frame', () => {
+    const bowlingGame = new Bowling();
+
+    for (let frame = 0; frame < 20; frame++) {
+      bowlingGame.score(frame, 1);
+    }
+
+    expect(bowlingGame.getScore()).toEqual(20);
   });
 });
--- src/app/models/bowling.ts
+++ src/app/models/bowling.ts
@@ -1,9 +1,19 @@
 export class Bowling {
 
+  public rolls: number[] = new Array<number>(21).fill(0);
+
   constructor() {
   }
 
   getScore() {
-    return 0;
+    let score = 0;
+    for (let roll = 0; roll < 21; roll++) {
+      score += this.rolls[roll];
+    }
+    return score;
+  }
+
+  score(frame: number, points: number) {
+    this.rolls[frame] = points;
   }
 }
