bcf13f8a80a5c7794510b5d8679769375f5f0f7c
Skander <besbes.skander@gmail.com>
2021-06-07 10:20:55 +0200

    Score 0

---
+++ src/app/services/bowling.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BowlingService {
+
+  constructor() { }
+
+  getScore() {
+    return 0;
+  }
+}
